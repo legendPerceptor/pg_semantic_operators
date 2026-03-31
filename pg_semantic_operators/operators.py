63bd903ef0089a5b26e85bf9cce41c5186716a50
Yuanjian Liu <achievinglegend@gmail.com>
2026-03-31 17:00:17 +0800

    feat: add audio operators to backward compat shim

--- pg_semantic_operators/operators.py
+++ pg_semantic_operators/operators.py
@@ -1,17 +1,18 @@
 # Backward compatibility shim - imports from new modular structure
-# This file is deprecated. Use: from pg_semantic_operators import ai_filter, ai_query, ai_image_filter, ai_image_describe
+# This file is deprecated. Use: from pg_semantic_operators import ai_filter, ai_query, ai_image_filter, ai_image_describe, ai_audio_filter, ai_audio_describe
 
 import warnings
 
 from .operators.ai_filter import ai_filter
 from .operators.ai_query import ai_query
 from .operators.ai_image import ai_image_filter, ai_image_describe
+from .operators.ai_audio import ai_audio_filter, ai_audio_describe
 
 warnings.warn(
     "Direct import from pg_semantic_operators.operators is deprecated. "
-    "Use 'from pg_semantic_operators import ai_filter, ai_query, ai_image_filter, ai_image_describe' instead.",
+    "Use 'from pg_semantic_operators import ai_filter, ai_query, ai_image_filter, ai_image_describe, ai_audio_filter, ai_audio_describe' instead.",
     DeprecationWarning,
     stacklevel=2
 )
 
-__all__ = ["ai_filter", "ai_query", "ai_image_filter", "ai_image_describe"]
+__all__ = ["ai_filter", "ai_query", "ai_image_filter", "ai_image_describe", "ai_audio_filter", "ai_audio_describe"]
